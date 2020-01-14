cddc9776fd2f3bdd33d8551c91a4873bc7240716
richarddempsey <richarddempsey@morrisonsplc.co.uk>
2020-01-14 07:40:39 +0000

    drag selection working

--- src/app/workflow-canvas/workflow-canvas.component.ts
+++ src/app/workflow-canvas/workflow-canvas.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-workflow-canvas',
   templateUrl: './workflow-canvas.component.html',
   styleUrls: ['./workflow-canvas.component.scss']
 })
-export class WorkflowCanvasComponent implements OnInit {
+export class WorkflowCanvasComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   tasks : Array<Task> = []
 
-  selected : Task;
+  selection : Array<Task> = []
+
+  isSelecting: boolean;
+  isDragging: boolean;
+  selectionStartX: number;
+  selectionStartY: number;
   
   newTask(){
     this.tasks.push(new Task(
@@ -25,26 +30,69 @@ export class WorkflowCanvasComponent implements OnInit {
   ngOnInit() {
   }
 
-  onPointerDown(e:PointerEvent){
-    this.tasks.forEach(t=>{
-      if(t.containsPoint(e.offsetX,e.offsetY)){
-        e.srcElement.setPointerCapture(e.pointerId);
-        this.selected = t;
-      }
-    })
+  @HostListener("window:resize", [])
+  private onResize() {
+    this.detectScreenSize();
   }
 
-  onPointerUp(e:PointerEvent){
-    if(this.selected){
-      this.selected = null;
-      e.srcElement.releasePointerCapture(e.pointerId);
+  ngAfterViewInit() {
+    this.detectScreenSize();
+  }
+
+  private detectScreenSize() {
+    // we will write this logic later
+  }
+
+  onPointerDown(e:PointerEvent){
+    this.isSelecting = false;
+    this.isDragging = false;
+
+    let hit : Task;
+    this.tasks.slice().reverse().forEach(t=>{
+      if( !hit && t.containsPoint(e.offsetX,e.offsetY)){
+        hit = t;
+      }
+    })
+
+    if(!hit){
+      this.isSelecting = true;
+      this.selection = [];
+      this.selectionStartX = e.offsetX;
+      this.selectionStartY = e.offsetY;
+    }
+    else{
+      this.isDragging = true;
+      if(this.selection.indexOf(hit)==-1){
+        this.selection = [];
+        this.selection.push(hit)
+      }
+      e.srcElement.setPointerCapture(e.pointerId);
     }
   }
 
+  onPointerUp(e:PointerEvent){
+    if( this.isDragging ){
+      e.srcElement.releasePointerCapture(e.pointerId);
+    }
+    this.isDragging = false;
+    this.isSelecting = false;
+  }
+
   onPointerMove(e:PointerEvent){
-    if( this.selected && e.buttons === 1 ){
-      this.selected.x = this.selected.x + e.movementX
-      this.selected.y = this.selected.y + e.movementY
+    if( this.isDragging ){
+      this.selection.forEach(t=>{
+        t.x = t.x + e.movementX
+        t.y = t.y + e.movementY
+      })
+    }
+    else if( this.isSelecting ){
+      this.tasks.forEach(t=>{
+        if(this.selection.indexOf(t)==-1){
+          if( t.isInsideRect(this.selectionStartX, this.selectionStartY, e.offsetX,e.offsetY)){
+            this.selection.push(t)
+          }
+        }
+      })
     }
   }
 }
@@ -58,4 +106,20 @@ export class Task {
   containsPoint(x:number,y:number):boolean{
     return x >= this.x && x <= (this.x + this.width) && y >= this.y && y<(this.y+this.height)
   }
+
+  isInsideRect(x0:number,y0:number,x1:number,y1:number):boolean{
+    if( x0>x1 ){
+      let tmp = x0;
+      x0=x1;
+      x1=tmp;
+    }
+    if( y0>y1 ){
+      let tmp = y0;
+      y0=y1;
+      y1=tmp;
+    }
+    return this.x >= x0 && (this.x+this.width)<=x1 && this.y >= y0 && (this.y+this.height)<=y1;
+  }
+
+
 }
