06673fa87e30e5cb258a6a87699ea539c06086ed
richarddempsey <richarddempsey@morrisonsplc.co.uk>
2020-01-17 14:13:12 +0000

    selected is now a property of node

--- src/app/workflow-canvas/workflow-canvas.component.ts
+++ src/app/workflow-canvas/workflow-canvas.component.ts
@@ -11,12 +11,12 @@ export class WorkflowCanvasComponent implements OnInit, AfterViewInit {
 
   nodes : Array<WorkflowNode> = []
 
-  selection : Array<WorkflowNode> = []
-
   isSelecting: boolean;
   isDragging: boolean;
   selectionStartX: number;
   selectionStartY: number;
+  selectionEndX: number;
+  selectionEndY: number;
   
   newTask(){
     this.nodes.push(new RectWorkflowNode(
@@ -64,25 +64,23 @@ export class WorkflowCanvasComponent implements OnInit, AfterViewInit {
     this.isSelecting = false;
     this.isDragging = false;
 
-    let hit : WorkflowNode;
-    this.nodes.slice().reverse().forEach(n=>{
-      if( !hit && n.containsPoint(e.offsetX,e.offsetY)){
-        hit = n;
-      }
-    })
+    let hit : WorkflowNode = this.nodes.slice().reverse().find(n=>n.containsPoint(e.offsetX,e.offsetY))
 
     if(!hit){
+      this.nodes.forEach(n=>n.selected=false);
       this.isSelecting = true;
-      this.selection = [];
       this.selectionStartX = e.offsetX;
       this.selectionStartY = e.offsetY;
+      this.selectionEndX = e.offsetX;
+      this.selectionEndY = e.offsetY;
     }
     else{
       this.isDragging = true;
-      if(this.selection.indexOf(hit)==-1){
-        this.selection = [];
-        this.selection.push(hit)
+      if(!hit.selected){
+        this.nodes.forEach(n=>n.selected=false);
+        hit.selected = true;
       }
+      hit.selected = true;
       e.srcElement.setPointerCapture(e.pointerId);
     }
   }
@@ -97,31 +95,44 @@ export class WorkflowCanvasComponent implements OnInit, AfterViewInit {
 
   onPointerMove(e:PointerEvent){
     if( this.isDragging ){
-      this.selection.forEach(n=>{
+      this.nodes.filter(n=>n.selected)
+      .forEach(n=>{
         n.x = n.x + e.movementX
         n.y = n.y + e.movementY
       })
     }
     else if( this.isSelecting ){
-      this.nodes.forEach(n=>{
-        if(this.selection.indexOf(n)==-1){
-          if( n.isInsideRect(this.selectionStartX, this.selectionStartY, e.offsetX,e.offsetY)){
-            this.selection.push(n)
-          }
-        }
-      })
+      this.selectionEndX = e.offsetX;
+      this.selectionEndY = e.offsetY
+      this.nodes.filter(n=> n.isInsideRect(this.selectionStartX, this.selectionStartY, e.offsetX,e.offsetY))
+        .forEach(n=>n.selected=true)
+        
     }
+  }
+
+  getSelectionStartX():number{
+    if( this.selectionStartX<=this.selectionEndX) return this.selectionStartX;
+    else return this.selectionEndX;
+  }
+
+  getSelectionStartY():number{
+    if( this.selectionStartY<=this.selectionEndY) return this.selectionStartY;
+    else return this.selectionEndY
+  }
+
+  getSelectionWidth():number{
+    let x :Array<number>= Utils.orderValues(this.selectionStartX,this.selectionEndX)
+    return Math.abs(x[1]-x[0]);
+  }
+
+  getSelectionHeight():number{
+    let y :Array<number>= Utils.orderValues(this.selectionStartY,this.selectionEndY)
+    return Math.abs(y[1]-y[0]);
   }
 }
 
-export abstract class WorkflowNode {
-
-  constructor(public x:number, public y: number ){}
-
-  abstract containsPoint(x:number,y:number):boolean
-  abstract isInsideRect(x0:number,y0:number,x1:number,y1:number):boolean
-
-  orderValues(n0:number,n1:number): Array<number>{
+class Utils {
+  static orderValues(n0:number,n1:number): Array<number>{
     if( n0>n1 ){
       let tmp = n0;
       n0=n1;
@@ -129,6 +140,16 @@ export abstract class WorkflowNode {
     }
     return [n0,n1]
   }
+}
+
+export abstract class WorkflowNode {
+
+  selected: boolean;
+
+  constructor(public x:number, public y: number ){}
+
+  abstract containsPoint(x:number,y:number):boolean
+  abstract isInsideRect(x0:number,y0:number,x1:number,y1:number):boolean
 }
 
 export class RectWorkflowNode extends WorkflowNode {
@@ -142,8 +163,8 @@ export class RectWorkflowNode extends WorkflowNode {
   }
 
   isInsideRect(x0:number,y0:number,x1:number,y1:number):boolean{
-    let x: Array<number> = this.orderValues(x0,x1)
-    let y: Array<number> = this.orderValues(y0,y1)
+    let x: Array<number> = Utils.orderValues(x0,x1)
+    let y: Array<number> = Utils.orderValues(y0,y1)
     return this.x >= x[0] && (this.x+this.width)<=x[1] && this.y >= y[0] && (this.y+this.height)<=y[1];
   }
 }
@@ -161,8 +182,8 @@ export class CircleWorkflowNode extends WorkflowNode {
   }
 
   isInsideRect(x0:number,y0:number,x1:number,y1:number):boolean{
-    let x: Array<number> = this.orderValues(x0,x1)
-    let y: Array<number> = this.orderValues(y0,y1)
+    let x: Array<number> = Utils.orderValues(x0,x1)
+    let y: Array<number> = Utils.orderValues(y0,y1)
     return x[0] <= (this.x-this.radius) && x[1] >= (this.x+this.radius) && y[0] <= (this.y-this.radius) && y[1] >= (this.y+this.radius)
   }
 }
@@ -202,8 +223,8 @@ export class DiamondWorkflowNode extends WorkflowNode {
   }
 
   isInsideRect(x0:number,y0:number,x1:number,y1:number):boolean{
-    let x: Array<number> = this.orderValues(x0,x1)
-    let y: Array<number> = this.orderValues(y0,y1)
+    let x: Array<number> = Utils.orderValues(x0,x1)
+    let y: Array<number> = Utils.orderValues(y0,y1)
     let X = this.x-(this.width/2)
     let Y = this.y-(this.height/2)
     return X >= x[0] && (X+this.width)<=x[1] && Y >= y[0] && (Y+this.height)<=y[1];
